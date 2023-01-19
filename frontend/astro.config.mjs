e8dc4bf44453251c3ea4d279ae8cd55aa2cb8e4f
Seigo <85787242+s3igo@users.noreply.github.com>
2023-01-19 21:23:19 +0900

    Feat/#13/articles (#21)
    * change[#17]: eslintの設定をFlat Configに
    
    * feat: 絶対パスによるインポート
    
    * add: spell checker
    
    * add: Content Collectionsを有効化
    
    * add: postsのfrontmatterのスキーマ
    
    * add: ESLint pluginに関する注釈

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Astro v2.0でexperimentalが外れる予定
+    experimental: { contentCollections: true },
+});
