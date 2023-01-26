5271df735bdb31be1220f10daa56c8c38bf8735b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-01-26 08:17:00 +0000

    feat[#18]: astro2.0へアップグレード
    `@astrojs/tailwind`がastro2.0に依存していたため

--- frontend/src/content/config.ts
+++ frontend/src/content/config.ts
@@ -1,12 +1,12 @@
 import { z, defineCollection } from "astro:content";
 
 const posts = defineCollection({
-    schema: {
+    schema: z.object({
         tags: z.array(z.string()),
         draft: z.literal(true).optional(),
         createdAt: z.date(),
         updatedAt: z.date().optional(),
-    },
+    }),
 });
 
 export const collections = { posts };
