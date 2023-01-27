9302913aac73b63f374c3aae3e0b0b706695f46f
s3igo <85787242+s3igo@users.noreply.github.com>
2023-01-27 06:48:25 +0000

    add: font

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            primary: "#011627",
+            secondary: "#FFEB95",
+            background: "#2F2F2F",
+            foreground: "#D6DEEB",
+            shadow: "#575656",
+        },
+        fontFamily: {
+            sans: ["Nunito Sans", "Zen Kaku Gothic New", "system-ui", "sans-serif"],
+            // code: ["Zen Kaku Gothic New", "sans-serif"],
+        },
+    },
+};
