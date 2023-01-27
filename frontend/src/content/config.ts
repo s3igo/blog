2093eff2e7305b773fb3927f42e17791d21ceda9
s3igo <85787242+s3igo@users.noreply.github.com>
2023-01-27 12:59:22 +0000

    change: update schema

--- frontend/src/content/config.ts
+++ frontend/src/content/config.ts
@@ -2,6 +2,7 @@ import { z, defineCollection } from "astro:content";
 
 const posts = defineCollection({
     schema: z.object({
+        layout: z.enum(["~/layouts/Blog.astro"]),
         tags: z.array(z.string()).optional(),
         draft: z.literal(true).optional(),
         createdAt: z.date(),
