ff325530bb65a06c5974cf6b5c4fa18782b7cc26
s3igo <85787242+s3igo@users.noreply.github.com>
2023-01-29 04:17:07 +0000

    feat: タイトルを正規化して抽出

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import remarkNormalizeHeadings from "remark-normalize-headings";
+import { remarkGetTitle } from "./src/plugins/remark-get-title.mjs";
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        remarkPlugins: [remarkNormalizeHeadings, remarkGetTitle],
+        extendedDefaultPlugins: true,
+    },
     integrations: [tailwind()],
 });
