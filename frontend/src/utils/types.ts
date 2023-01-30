90bfd55308b652bcd7b70344fd65b8be959a1b39
tsukiyo <85787242+s3igo@users.noreply.github.com>
2023-01-30 13:02:42 +0900

    Fix/#34/content directory (#36)
    * remove: content collection
    
    * change: filename
    
    * feat: plugins
    
    * fix: import path
    
    * wip: blog layout

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,20 @@
+import { z } from "astro/zod";
+
+// @types/mdastに型あるけど、そこまで厳密性必要ないため使わない
+type Func = (tree: any, file: any) => void;
+export type Plugin = () => Func;
+
+// レイアウトの種類
+export const layouts = {
+    Blog: "~/layouts/Blog.astro",
+} as const;
+
+// frontmatterの型定義
+export const frontmatterSchema = z.object({
+    layout: z.literal(layouts.Blog), // 複数になったらz.union()にする
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    draft: z.literal(true).optional(),
+    createdAt: z.date(),
+    updatedAt: z.date().optional(),
+});
