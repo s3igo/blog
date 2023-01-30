a03a70cb7dc1b299746dafada92d4742e0e0f9a7
tsukiyo <85787242+s3igo@users.noreply.github.com>
2023-01-30 13:02:42 +0900

    Fix/#34/content directory (#36)
    * remove: content collection
    
    * change: filename
    
    * feat: plugins
    
    * fix: import path
    
    * wip: blog layout

---
+++ frontend/src/utils/remarkPlugins/validateFrontmatter.ts
@@ -0,0 +1,10 @@
+import type { Plugin } from "../types";
+import { frontmatterSchema as schema } from "../types";
+
+// frontmatterの型チェック
+export const validateFrontmatter: Plugin = () => {
+    return (_, { data }) => {
+        const frontmatter = data.astro.frontmatter;
+        schema.parse(frontmatter);
+    };
+};
