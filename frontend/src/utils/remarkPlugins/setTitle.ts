d20108ebc755d8e332a087e94e8fa4b26686b05c
tsukiyo <85787242+s3igo@users.noreply.github.com>
2023-02-02 12:44:58 +0900

    Feat/#27/index (#39)
    * change: 冗長な記述
    
    * change: `Astro.glob()`を使って取得する実装に
    
    * change: `Astro.glob()`を使うことによる抽出処理の置き換え
    
    * change: file name
    
    * wip
    
    * change: 分割代入をフォーマット
    
    * remove: `README` in `./frontend`

--- frontend/src/utils/remarkPlugins/setTitle.ts
+++ frontend/src/utils/remarkPlugins/setTitle.ts
@@ -2,12 +2,12 @@ import type { Plugin } from "../types";
 
 // h1タグの内容をfrontmatter.titleにセットする
 export const setTitle: Plugin = () => {
-    return ({ children }, { data }) => {
+    return ({ children }, { data: { astro: frontmatter } }) => {
         // ASTからh1タグを探す
         // remark-normalize-headingsプラグインにより、h1タグは必ず1つ存在することが保証されている
         const title = children.find(
             ({ type, depth }: { type: string; depth: number }) => type === "heading" && depth === 1
         ).children[0].value;
-        data.astro.frontmatter.title = title;
+        frontmatter.title = title;
     };
 };
