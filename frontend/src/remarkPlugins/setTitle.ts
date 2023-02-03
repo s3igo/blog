337d4e484c53c2d308e792f11b28e23eb0010993
tsukiyo <85787242+s3igo@users.noreply.github.com>
2023-02-04 00:28:12 +0900

    Feat/#37/card (#40)
    * chore: 冗長なコメントを削除
    
    * fix: remarkpluginの分割代入
    
    * remove: `page` ページネーション必要になってから
    
    * change: file path
    
    * add: line-clamp plugin
    
    * wip
    
    * change: update tailwind colors
    
    * add: subtext color
    
    * feat: ロジックを切り出し
    
    * feat: `Card` component

--- frontend/src/remarkPlugins/setTitle.ts
+++ frontend/src/remarkPlugins/setTitle.ts
@@ -2,7 +2,14 @@ import type { Plugin } from "../types";
 
 // h1タグの内容をfrontmatter.titleにセットする
 export const setTitle: Plugin = () => {
-    return ({ children }, { data: { astro: frontmatter } }) => {
+    return (
+        { children },
+        {
+            data: {
+                astro: { frontmatter },
+            },
+        }
+    ) => {
         // ASTからh1タグを探す
         // remark-normalize-headingsプラグインにより、h1タグは必ず1つ存在することが保証されている
         const title = children.find(
