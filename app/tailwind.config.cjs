89c0ab0142bd06e847e1a99bc30cb7cfb90ed418
s3igo <85787242+s3igo@users.noreply.github.com>
2023-02-15 11:13:14 +0000

    add: xs breakpoint

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,19 +1,24 @@
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
     theme: {
         colors: {
-            transparent: "transparent",
-            background: "#011627",
-            foreground: "#D6DEEB",
-            primary: "#FFEB95",
-            secondary: "#7fdbca",
-            tertiary: "#82aaff",
-            shadow: "#575656",
+            transparent: 'transparent',
+            background: '#011627',
+            foreground: '#D6DEEB',
+            primary: '#FFEB95',
+            secondary: '#7fdbca',
+            tertiary: '#82aaff',
+            shadow: '#575656',
         },
         fontFamily: {
-            sans: ["Nunito Sans", "Zen Kaku Gothic New", "system-ui", "sans-serif"],
-            code: ["JetBrains Mono", "BIZ UDGothic", "monospace", "sans-serif"],
+            sans: ['Nunito Sans', 'Zen Kaku Gothic New', 'system-ui', 'sans-serif'],
+            code: ['JetBrains Mono', 'BIZ UDGothic', 'monospace', 'sans-serif'],
+        },
+        extend: {
+            screens: {
+                xs: '375px',
+            },
         },
     },
-    plugins: [require("@tailwindcss/line-clamp")],
+    plugins: [require('@tailwindcss/line-clamp')],
 };
