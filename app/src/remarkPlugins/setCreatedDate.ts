782dfef08777a0415ab6e74f7a83d774d562485b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-02-22 10:13:55 +0000

    chore: change var-name

--- app/src/remarkPlugins/setCreatedDate.ts
+++ app/src/remarkPlugins/setCreatedDate.ts
@@ -3,7 +3,7 @@ import type { Plugin } from './types';
 // ファイル名から作成日を取得してfrontmatter.createdAtにセットする
 export const setCreatedDate: Plugin = () => {
     return (_, { data, history: [path] }) => {
-        const [date] = path.split('/').slice(-2);
-        data.astro.frontmatter.createdAt = new Date(date);
+        const [dateStr] = path.split('/').slice(-2);
+        data.astro.frontmatter.createdAt = new Date(dateStr);
     };
 };
