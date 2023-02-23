0293cefa6dbcf46995135b86e8a9a34e37162317
s3igo <85787242+s3igo@users.noreply.github.com>
2023-02-23 11:54:40 +0000

    test: update playwright config

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     webServer: {
         command: 'npm run preview',
         url: 'http://localhost:3000',
+        port: 3000,
         reuseExistingServer: !process.env.CI,
     },
     use: {
