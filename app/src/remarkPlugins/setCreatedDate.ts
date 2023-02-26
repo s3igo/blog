015e0c473fa21c47fcf5fded496cc6061a5677c0
s3igo <85787242+s3igo@users.noreply.github.com>
2023-02-26 11:32:25 +0000

    remove: いらないファイル

--- app/src/remarkPlugins/setCreatedDate.ts
+++
@@ -1,9 +0,0 @@
-import type { Plugin } from './types';
-
-// ファイル名から作成日を取得してfrontmatter.createdAtにセットする
-export const setCreatedDate: Plugin = () => {
-    return (_, { data, history: [path] }) => {
-        const [dateStr] = path.split('/').slice(-2);
-        data.astro.frontmatter.createdAt = new Date(dateStr);
-    };
-};
