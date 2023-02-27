c217f58f6fc827f5aec641e19733813eec40ffb3
s3igo <85787242+s3igo@users.noreply.github.com>
2023-02-27 03:32:38 +0000

    change: file name

--- app/src/remarkPlugins/setPubDate.ts
+++
@@ -1,9 +0,0 @@
-import type { Plugin } from './types';
-
-// ファイル名から作成日を取得してfrontmatter.pubDateにセットする
-export const setPubDate: Plugin = () => {
-    return (_, { data, history: [path] }) => {
-        const [dateStr] = path.split('/').slice(-2);
-        data.astro.frontmatter.pubDate = new Date(dateStr);
-    };
-};
