b9b89e21dfc0682bc18ac5dde5ac913d519a369e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-02-27 08:04:03 +0000

    change: playwright config

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     webServer: {
         command: 'npm run preview',
+        url: 'http://localhost:3000/',
         reuseExistingServer: !process.env.CI,
         timeout: 120 * 1000,
     },
