b60d5f21b263e73172419e438f2e1461c3591ec1
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-01 04:38:09 +0000

    change: filename

--- app/src/utils/constructString.ts
+++ app/src/utils/constructString.ts
@@ -1,3 +1,7 @@
 import { PAGE_TITLE } from '~/constants';
 
 export const pageTitle = (text: string): string => `${text} | ${PAGE_TITLE}`;
+
+// TODO: ほかの関数もここに移す
+
+export const tagUrl = (name: string): string => `/tags/${name}`;
