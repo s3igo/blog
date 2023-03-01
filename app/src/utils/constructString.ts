eb4a6bd43690f35f29156cbacded00a6cbba4b88
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-01 05:15:34 +0000

    change: remove いらない関数

--- app/src/utils/constructString.ts
+++ app/src/utils/constructString.ts
@@ -3,5 +3,3 @@ import { PAGE_TITLE } from '~/constants';
 export const pageTitle = (text: string): string => `${text} | ${PAGE_TITLE}`;
 
 export const postUrl = (date: string, slug: string): string => `/posts/${date}/${slug}`;
-
-export const tagUrl = (name: string): string => `/tags/${name}`;
