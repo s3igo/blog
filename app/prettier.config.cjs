7e326d57fe7952671db7b775d229a41cf22e8809
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-02 23:56:38 +0000

    change: delete import order config

--- app/prettier.config.cjs
+++ app/prettier.config.cjs
@@ -1,6 +1,3 @@
 module.exports = {
     singleQuote: true,
-    importOrder: ['<THIRD_PARTY_MODULES>', '^[~/]', '^[./]'],
-    importOrderSortSpecifiers: true,
-    importOrderCaseInsensitive: true,
 };
