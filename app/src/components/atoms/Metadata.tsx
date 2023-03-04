0cd6e7e6430b5c4ba8a12579b51466ca3bf86804
s3igo <s3igo@users.noreply.github.com>
2023-03-04 04:11:52 +0000

    Apply ESLint Change

--- app/src/components/atoms/Metadata.tsx
+++ app/src/components/atoms/Metadata.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js';
 
 export type Props = { text: string; block?: boolean };
-export const Metadata: Component<Props> = ({ text, block = false }) => (
+export const Metadata: Component<Props> = ({ block = false, text }) => (
     <span class="text-sm text-foreground/60 sm:text-base truncate" classList={{ block }}>
         {text}
     </span>
