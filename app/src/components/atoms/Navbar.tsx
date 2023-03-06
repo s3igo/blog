46b3e98aadb97380bc29af57422edb869e8e29d6
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-06 04:57:48 +0000

    chore: minor update

--- app/src/components/atoms/Navbar.tsx
+++ app/src/components/atoms/Navbar.tsx
@@ -7,7 +7,7 @@ export type Props = { isH1: boolean };
 export const Navbar: Component<Props> = (props) => (
     <nav
         class="custom-shadow flex h-14 items-center justify-between rounded-full border-2 border-solid border-primary bg-background xs:h-16 sm:h-20"
-        aria-labelledby="Primary navigation"
+        aria-label="Global navigation"
     >
         <a class="pl-5 text-2xl text-primary xs:pl-6 xs:text-3xl sm:pl-8 sm:text-[40px]" href="/">
             <Show when={props.isH1} fallback={PAGE_TITLE}>
