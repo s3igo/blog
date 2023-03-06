6a14d7565513c39723d7cb1a1bba930885137e16
s3igo <85787242+s3igo@users.noreply.github.com>
2023-03-06 10:14:48 +0000

    change: refactor

---
+++ app/src/utils/types.ts
@@ -0,0 +1,13 @@
+export type ExcessPropertyCheck<T extends object, TExpected extends object> = Exclude<
+    keyof T,
+    keyof TExpected
+> extends never
+    ? T
+    : never;
+
+export type Prettify<T extends object> = { [K in keyof T]: T[K] };
+
+export type EmptyObject = { [key: string]: never };
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const isNever = <_ extends never>(): void => {};
