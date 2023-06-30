f11608b9483346932d6bd5a5a0694336608a204e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-01 02:50:29 +0900

    いくつか型を追加

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -9,3 +9,30 @@ export type Prettify<T extends object> = { [K in keyof T]: T[K] };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const isNever = <_ extends never>(): void => {};
+
+export type Companion<T, U> = {
+    new: (value: T) => U;
+};
+
+// usecase: type T = string & { readonly brand: unique symbol };
+export const newType = <T, U extends T & { readonly brand: symbol }>(value: T): U =>
+    value as unknown as U;
+
+if (import.meta.vitest) {
+    const { describe, expect, test } = import.meta.vitest;
+
+    describe('newType', () => {
+        test('string', () => {
+            const a = newType('a');
+            const b = newType('b');
+            expect(a).toBe('a');
+            expect(b).toBe('b');
+        });
+        test('number', () => {
+            const a = newType(1);
+            const b = newType(2);
+            expect(a).toBe(1);
+            expect(b).toBe(2);
+        });
+    });
+}
