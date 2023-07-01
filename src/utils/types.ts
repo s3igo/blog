bfaa8895a89d9e269177409c9ee83233cad6ce3f
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-01 15:02:54 +0900

    使ってないと思われるやつをコメントアウト

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,14 +1,14 @@
-export type ExcessPropertyCheck<T extends object, TExpected extends object> = Exclude<
-    keyof T,
-    keyof TExpected
-> extends never
-    ? T
-    : never;
+// export type ExcessPropertyCheck<T extends object, TExpected extends object> = Exclude<
+//     keyof T,
+//     keyof TExpected
+// > extends never
+//     ? T
+//     : never;
 
 export type Prettify<T extends object> = { [K in keyof T]: T[K] };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export const isNever = <_ extends never>(): void => {};
+// export const isNever = <_ extends never>(): void => {};
 
 export type Companion<T, U> = {
     new: (value: T) => U;
