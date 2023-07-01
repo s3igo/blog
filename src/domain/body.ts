acf0f9e665a6f6fd3fd4426ee889acf724c31c05
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-01 16:29:52 +0900

    first3sentencesを追加

--- src/domain/body.ts
+++ src/domain/body.ts
@@ -1,11 +1,16 @@
-import { truncate } from '~/utils/string';
+import { first3Sentences, truncate } from '~/utils/string';
 import { newType } from '~/utils/types';
 import type { Companion } from '~/utils/types';
 
 type BodySchema = {
+    /** 記事のタイトル */
     title: string;
+    /** 記事の本文 */
     content: string;
+    /** 記事の概要（graphemeベースで500文字） */
     description: string;
+    /** 記事の最初の3文 */
+    firstThreeSentences: string;
 };
 
 export type Body = BodySchema & { readonly brand: unique symbol };
@@ -17,6 +22,7 @@ const transformBody = (value: string): BodySchema => {
     return {
         content: body.join(''),
         description: truncate(body.join(''), 500),
+        firstThreeSentences: first3Sentences(body.join('')),
         title: title || 'No Title',
     };
 };
@@ -58,6 +64,16 @@ if (import.meta.vitest) {
         });
         test('title with no title', () => {
             expect(Body.new('content').title).toBe('No Title');
+        });
+    });
+    describe('firstThreeSentences', () => {
+        test('firstThreeSentences', () => {
+            expect(Body.new('# title\ncontent').firstThreeSentences).toBe('content');
+        });
+        test('firstThreeSentences with 3文より長い場合', () => {
+            expect(Body.new('# title\nあいう。えお。かきく。けこ。').firstThreeSentences).toBe(
+                'あいう。えお。かきく。'
+            );
         });
     });
 }
