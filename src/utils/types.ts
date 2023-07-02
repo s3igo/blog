6fb975579b7bed2d30aa03d78e4cfe00c7a49c44
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-02 21:21:39 +0900

    NoInfer型とOpaqueユーティリティ

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -10,11 +10,51 @@ export type Prettify<T extends object> = { [K in keyof T]: T[K] };
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 // export const isNever = <_ extends never>(): void => {};
 
+/**
+ * コンパニオンオブジェクトパターンでコンストラクタを注釈するための型
+ * @param T コンストラクタの引数の型
+ * @param U コンストラクタの戻り値の型
+ */
 export type Companion<T, U> = {
     new: (value: T) => U;
 };
 
-// ex: type T = string & { readonly brand: unique symbol };
+/**
+ * 型引数を省略した場合に推論させない型
+ * @param T 対象の型
+ * @example const fn = <T>(value: NoInfer<T>): T => value;
+ *     const a = fn('a'); // Error
+ *     const b = fn<string>('b'); // OK
+ */
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
+/**
+ * Opaque型を作成するための型
+ * @param T Opaque型の元になる型
+ * @param U Opaque型のブランド
+ * @example type A = Opaque<string, 'A'>;
+ */
+export type Opaque<T, U extends string> = T & { readonly _brand: U };
+
+/**
+ * Opaque型を作成するためのユーティリティ
+ * @example const a = Opaque.create<A, string>('a');
+ *     type B = Opaque<string, 'B'>;
+ *     const b: B = a; // Error
+ *     const c: string = a; // OK
+ */
+export const Opaque = {
+    /**
+     * Opaque型を作成するコンストラクター
+     * @param T Opaque<T, U>によって作成された型
+     * @param U 引数vに渡す型
+     * @param v 作成する型の値
+     * @example const a = Opaque.create<A, string>('a');
+     */
+    create: <T extends Opaque<U, string>, U = never>(v: NoInfer<U>): T => v as unknown as T,
+};
+
+// ex: type T = string & { readonly brand: unique symbol }; //
 export const newType = <T, U extends T & { readonly brand: symbol }>(v: T): U => v as unknown as U;
 
 if (import.meta.vitest) {
