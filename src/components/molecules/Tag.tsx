a32c09b2be0f32b790f98ca5805f9e7987d0d82b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-02 09:15:52 +0900

    tagコンポーネントをシンプルに

--- src/components/molecules/Tag.tsx
+++ src/components/molecules/Tag.tsx
@@ -2,21 +2,20 @@ import { type Component, mergeProps } from 'solid-js';
 import { Metadata } from '../atoms/Metadata';
 
 type Props = { name: string; truncate?: boolean };
-type ViewProps = { url: string } & Required<Props>;
-
-const View: Component<ViewProps> = (props) => (
-    <a
-        href={props.url}
-        target="_blank"
-        rel="noopener"
-        class="hover:custom-shadow ml-[-6px] rounded-md border-2 border-transparent px-1 hover:border-secondary w-min max-w-full"
-    >
-        <Metadata text={`#${props.name}`} block={props.truncate} />
-    </a>
-);
 
 export const Tag: Component<Props> = (props) => {
-    const local = mergeProps({ truncate: false }, props);
-    const view = { ...local, url: `/tags/${local.name}` };
-    return <View {...view} />;
+    const propsWithDefaultValue = mergeProps({ truncate: false }, props);
+    return (
+        <a
+            href={`/tags/${propsWithDefaultValue.name}`}
+            target="_blank"
+            rel="noopener"
+            class="hover:custom-shadow ml-[-6px] rounded-md border-2 border-transparent px-1 hover:border-secondary w-min max-w-full"
+        >
+            <Metadata
+                text={`#${propsWithDefaultValue.name}`}
+                block={propsWithDefaultValue.truncate}
+            />
+        </a>
+    );
 };
