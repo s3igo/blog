9a047f10cf197b47d0e2e902a04b728f1df456cc
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-03 11:38:19 +0900

    datesをリファクタ

--- src/domain/model/post/dates.ts
+++ src/domain/model/post/dates.ts
@@ -2,14 +2,13 @@ import type { Schema as FrontmatterSchema } from '~/content/config';
 import { type Companion, Opaque } from '~/utils/types';
 import { DateString } from './dates/dateString';
 
+/** @package */
+export { type DateString };
+
 type Receive = Pick<FrontmatterSchema, 'published' | 'updated'>;
 
 type DatesSchema = {
-    /** 記事の公開日（Date型） */
-    rawPublished: Date;
-    /** 記事の公開日 */
     published: DateString;
-    /** 記事の更新日 */
     updated?: DateString | undefined;
 };
 
@@ -19,7 +18,6 @@ export const Dates: Companion<Receive, Dates> = {
     new: ({ published, updated }) =>
         Opaque.create<Dates, DatesSchema>({
             published: DateString.new(published),
-            rawPublished: published,
             updated: updated ? DateString.new(updated) : undefined,
         }),
 };
@@ -40,10 +38,6 @@ if (import.meta.vitest) {
             });
             expect(dates.published).toBe('2021-01-01');
             expect(dates.updated).toBe('2021-01-02');
-        });
-        test('rawPublished', () => {
-            const dates = Dates.new({ published: new Date('2021-01-01'), updated: null });
-            expect(dates.rawPublished).toEqual(new Date('2021-01-01'));
         });
     });
 }
