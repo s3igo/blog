c9ff2e6db143018c2089e4f7068fa5672c06b983
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-05 10:24:15 +0900

    `constructString`を削除

--- src/utils/constructString.ts
+++
@@ -1,5 +0,0 @@
-import { PAGE_TITLE } from '~/utils/constants';
-
-export const pageTitle = (text: string): string => `${text} | ${PAGE_TITLE}`;
-
-// export const postUrl = (date: string, slug: string): string => `/posts/${date}/${slug}`;
