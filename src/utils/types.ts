5186d59aa84ca1f56d949eefb254a43ca486d662
tsukiyo <85787242+s3igo@users.noreply.github.com>
2023-07-06 19:20:53 +0900

    Refactor/#234/opaque (#235)
    * 共通部分を整理
    
    * InferBrandを作成

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -10,6 +10,12 @@ export type Companion<T, U> = {
     new: (value: T) => U;
 };
 
+/** Brandの名前 */
+type BrandName<T extends string> = `_${T}Brand`;
+
+/** never型が露出しないためのtype alias */
+type BrandMarker = never;
+
 /**
  * Opaque型を作成する
  * @param T Opaque型の元になる型
@@ -17,8 +23,16 @@ export type Companion<T, U> = {
  * @example type A = Opaque<string, 'A'>;
  */
 export type Opaque<T extends NonNullable<unknown>, U extends string> = T & {
-    [P in U as `_${P}Brand`]: never;
+    [P in U as BrandName<U>]: BrandMarker;
 };
+
+// TODO: Brandが複数存在する場合に対応
+// Brandが複数存在する場合、Brandを作成することはできるがそこから元の型を取り出すことができない
+
+/** Opaque型からBrandを取り出す */
+type InferBrand<T> = {
+    [K in keyof T]: T[K] extends BrandMarker ? K : never;
+}[keyof T]; //=> string | number | symbol
 
 /**
  * Opaque型とそれに付与したBrandから元の型を取り出す
@@ -27,7 +41,7 @@ export type Opaque<T extends NonNullable<unknown>, U extends string> = T & {
  * @example type A = Opaque<string, 'A'>;
  *     type B = InferBase<A, 'A'>; //=> string
  */
-type InferBase<T, U extends string> = T extends infer V & { [K in `_${U}Brand`]: never }
+type InferBase<T, U extends string> = T extends infer V & { [K in BrandName<U>]: BrandMarker }
     ? V
     : never;
 
@@ -40,17 +54,17 @@ type InferBase<T, U extends string> = T extends infer V & { [K in `_${U}Brand`]:
 export const Opaque = {
     /**
      * Opaque型の値を作成する
-     * @constructor
+     * @function
      * @param T Opaque<T, U>によって作成した型
      * @param U Opaque<T, U>によって作成した型のBrand
-     * @param v 作成する型の値
+     * @param value 作成する型の値
      * @example type A = Opaque<string, 'A'>;
      *     const a = Opaque.create<A, 'A'>('foo');
      *     type B = Opaque<string, 'B'>;
      *     const b: B = a; // Error
      *     const c: string = a; // OK
      */
-    create: <T, U extends string>(v: InferBase<T, U>) => v as unknown as T,
+    create: <T, U extends string>(value: InferBase<T, U>) => value as T,
 };
 
 if (import.meta.vitest) {
