f3ec5119a35e53b42a01e6d13936d29978612c0e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-07 22:36:47 +0900

    mjs -> tsに変更

--- astro.config.ts
+++ astro.config.ts
@@ -4,18 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            // カスタムクラスに`:hover`などのバリアントを適用するために必要
-            // ref: https://github.com/withastro/astro/issues/3844
-            config: {
-                applyBaseStyles: false,
-            },
-        }),
-        solidJs(),
-    ],
+    integrations: [tailwind({ applyBaseStyles: false }), solidJs()],
     markdown: {
-        extendedDefaultPlugins: true,
         remarkPlugins: ['remark-code-titles'],
         shikiConfig: {
             theme: 'material-theme-ocean',
