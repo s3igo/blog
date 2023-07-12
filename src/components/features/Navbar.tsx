979ff558e87d80c73b7d35036736569a77d03be1
s3igo <85787242+s3igo@users.noreply.github.com>
2023-07-12 21:48:22 +0900

    feat: ダークモードをトグルするロジック

--- src/components/features/Navbar.tsx
+++ src/components/features/Navbar.tsx
@@ -1,5 +1,5 @@
 import { As, Button } from '@kobalte/core';
-import type { Component } from 'solid-js';
+import { type Component, createSignal } from 'solid-js';
 import { PAGE_TITLE } from '~/utils/constants';
 
 const RssIcon: Component = () => (
@@ -16,22 +16,72 @@ const RssIcon: Component = () => (
     </svg>
 );
 
-export const Navbar: Component = () => (
-    <nav
-        class="flex h-14 items-center justify-between rounded-full border-2 border-solid xl:h-20  bg-white relative"
-        aria-label="Global navigation"
+const DarkModeIcon: Component = () => (
+    <svg
+        fill="none"
+        stroke-width="0"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        height="1em"
+        width="1em"
+        style="overflow: visible;"
     >
-        <a class="absolute left-6 lg:left-8 text-accent text-3xl xl:text-[40px]" href="/">
-            <h1>{PAGE_TITLE}</h1>
-        </a>
-        <Button.Root asChild>
-            <As
-                component="a"
-                href="/rss.xml"
-                class="absolute xl:h-12 xl:w-12 h-8 w-8 right-3 xl:right-4 border-2 border-transparent rounded-full hover:border-cyan-500 p-1 xl:p-2"
-            >
-                <RssIcon />
-            </As>
-        </Button.Root>
-    </nav>
+        <path fill="currentColor" d="M12 16a4 4 0 0 0 0-8v8Z"></path>
+        <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2Zm0 2v4a4 4 0 1 0 0 8v4a8 8 0 1 0 0-16Z"
+            clip-rule="evenodd"
+        ></path>
+    </svg>
 );
+
+export const Navbar: Component = () => {
+    const getCurrentTheme = (): 'light' | 'dark' => {
+        if (typeof window.localStorage.getItem('theme') === 'string') {
+            const current = window.localStorage.getItem('theme');
+            return current === 'dark' ? 'dark' : 'light';
+        } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            return 'dark';
+        } else {
+            return 'light';
+        }
+    };
+
+    const [darkMode, setDarkMode] = createSignal(getCurrentTheme() === 'dark' ? true : false);
+
+    const toggleTheme = (): void => {
+        if (darkMode()) {
+            document.documentElement.classList.remove('dark');
+            window.localStorage.setItem('theme', 'light');
+            setDarkMode(false);
+        } else {
+            document.documentElement.classList.add('dark');
+            window.localStorage.setItem('theme', 'dark');
+            setDarkMode(true);
+        }
+    };
+
+    return (
+        <nav
+            class="flex h-14 items-center justify-between rounded-full border-2 border-solid xl:h-20  bg-white relative"
+            aria-label="Global navigation"
+        >
+            <a class="absolute left-6 lg:left-8 text-accent text-3xl xl:text-[40px]" href="/">
+                <h1>{PAGE_TITLE}</h1>
+            </a>
+            <Button.Root asChild>
+                <As
+                    component="a"
+                    href="/rss.xml"
+                    class="absolute xl:h-12 xl:w-12 h-8 w-8 right-3 xl:right-4 border-2 border-transparent rounded-full hover:border-cyan-500 p-1 xl:p-2"
+                >
+                    <RssIcon />
+                </As>
+            </Button.Root>
+            <Button.Root onClick={toggleTheme}>
+                <DarkModeIcon />
+            </Button.Root>
+        </nav>
+    );
+};
