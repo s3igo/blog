ef9b0b33ff39a268bb4deba8fecb65218019e118
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-28 22:56:01 +0900

    feat: change file name

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    importOrder: ['^@(.*)$', '<THIRD_PARTY_MODULES>', '^~(.*)$', '^../(.*)$', '^./(.*)$'],
+    importOrderCaseInsensitive: true,
+    importOrderGroupNamespaceSpecifiers: true,
+    importOrderSortSpecifiers: true,
+    jsonRecursiveSort: true,
+    singleQuote: true,
+};
