c492042d4d3e4d9d640dcded70882f70419f5cea
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-28 23:17:42 +0900

    feat: update prettier-plugin-astro

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,19 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-    importOrder: ['^@(.*)$', '<THIRD_PARTY_MODULES>', '^~(.*)$', '^../(.*)$', '^./(.*)$'],
-    importOrderCaseInsensitive: true,
-    importOrderGroupNamespaceSpecifiers: true,
-    importOrderSortSpecifiers: true,
+    // importOrder: ['^@(.*)$', '<THIRD_PARTY_MODULES>', '^~(.*)$', '^../(.*)$', '^./(.*)$'],
+    // importOrderCaseInsensitive: true,
+    // importOrderGroupNamespaceSpecifiers: true,
+    // importOrderSortSpecifiers: true,
     jsonRecursiveSort: true,
+    jsonSortOrder: 'lexical',
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+    plugins: ['prettier-plugin-astro', 'prettier-plugin-sort-json'],
     singleQuote: true,
 };
