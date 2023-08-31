7d0ee53ed8113dcda7436a0edef5f96aaca7586d
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-31 18:34:47 +0900

    astro-compressを追加

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,17 @@
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ applyBaseStyles: false }), solidJs()],
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        solidJs(),
+        compress(),
+    ],
     markdown: {
         remarkPlugins: ['remark-code-titles'],
         shikiConfig: {
