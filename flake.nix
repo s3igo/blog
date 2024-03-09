74a0aed09259fe6dfa576f349e8f9f7528792349
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-09 14:25:10 +0900

    feat: add neovim

--- flake.nix
+++ flake.nix
@@ -2,22 +2,43 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dotfiles.url = "github:s3igo/dotfiles";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      nixpkgs,
+      flake-utils,
+      dotfiles,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        neovim = dotfiles.neovim.${system} {
+          inherit pkgs;
+          modules = with dotfiles.nixosModules; [
+            im-select
+            nix
+            # typescript
+          ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            statix
-            nodejs-slim
-            nodePackages.pnpm
-          ];
+          buildInputs =
+            with pkgs;
+            [
+              statix
+              nodejs-slim
+              nodePackages.pnpm
+            ]
+            ++ [ neovim ];
+        };
+
+        packages = {
+          inherit neovim;
         };
 
         formatter = pkgs.nixfmt-rfc-style;
