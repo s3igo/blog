f28c1055dbb37b2560994ae409077cc69b96dc8f
Seigo Mori <85787242+s3igo@users.noreply.github.com>
2024-03-10 13:22:41 +0900

    Refactor (#469)
    * feat: update editor
    
    * chore: remove unused file
    
    * feat: add pull request action
    
    * Apply Format and Lint changes
    
    * fix: remove format
    
    ---------
    
    Co-authored-by: s3igo <s3igo@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -22,6 +22,103 @@
             im-select
             nix
             # typescript
+            (
+              { pkgs, ... }:
+
+              {
+                plugins = {
+                  treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+                    javascript
+                    jsdoc
+                    typescript
+                    tsx
+                    regex
+                  ];
+                  lsp.servers.tsserver.enable = true;
+                  none-ls = {
+                    enable = true;
+                    sources.formatting.prettier = {
+                      enable = true;
+                      disableTsServerFormatter = true;
+                    };
+                  };
+                  ts-autotag.enable = true;
+                  ts-context-commentstring.enable = true;
+                };
+              }
+            )
+            # json
+            (
+              { pkgs, ... }:
+
+              {
+                plugins = {
+                  treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+                    json
+                    json5
+                    regex
+                  ];
+                  lsp.servers.jsonls.enable = true;
+                };
+              }
+            )
+            # prettier
+            {
+              plugins = {
+                none-ls = {
+                  enable = true;
+                  sources.formatting.prettier = {
+                    enable = true;
+                    disableTsServerFormatter = true;
+                  };
+                };
+              };
+            }
+            # yaml
+            (
+              { pkgs, ... }:
+
+              {
+                plugins = {
+                  treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+                    yaml
+                    regex
+                  ];
+                  lsp.servers.yamlls.enable = true;
+                };
+              }
+            )
+            # markdown
+            (
+              { pkgs, ... }:
+
+              {
+                plugins = {
+                  treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+                    markdown
+                    markdown_inline
+                    regex
+                  ];
+                };
+              }
+            )
+            # project specific
+            (
+              { pkgs, ... }:
+              {
+                plugins = {
+                  treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+                    astro
+                    css
+                  ];
+                  lsp.servers = {
+                    eslint.enable = true;
+                    astro.enable = true;
+                    tailwindcss.enable = true;
+                  };
+                };
+              }
+            )
           ];
         };
       in
