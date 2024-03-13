2afa2d39f03b2b71f3127209387accabc2d8285f
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-13 14:58:16 +0900

    feat: add remarkplugin

--- astro.config.ts
+++ astro.config.ts
@@ -3,18 +3,17 @@ import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
+import { remarkStripLineBreaks } from './remark-strip-line-breaks';
 
 export default defineConfig({
     integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
+        tailwind({ applyBaseStyles: false }),
         solid(),
         compress(),
         sitemap(),
     ],
     markdown: {
-        remarkPlugins: ['remark-code-titles'],
+        remarkPlugins: [remarkStripLineBreaks, 'remark-code-titles'],
         shikiConfig: {
             themes: {
                 dark: 'material-theme',
@@ -22,8 +21,6 @@ export default defineConfig({
             },
         },
     },
-    prefetch: {
-        prefetchAll: true,
-    },
+    prefetch: { prefetchAll: true },
     site: 'https://blog.tsuki-yo.net',
 });
