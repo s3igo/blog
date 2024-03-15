706ef62f97cbcbdd603077bed906b96d10e8739f
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-15 15:02:55 +0900

    refactor: remove domain

---
+++ src/components/features/Navbar/Themebutton/icon/sunny-icon.tsx
@@ -0,0 +1,78 @@
+import { component$, useSignal } from '@builder.io/qwik';
+
+export const SunnyIcon = component$(() => {
+    const animateRef = useSignal<SVGAnimateElement>();
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            viewBox="0 0 24 24"
+            onMouseEnter$={() => animateRef.value?.beginElement()}
+        >
+            <animate
+                id="sunny-filled-entrypoint"
+                begin="indefinite"
+                ref={animateRef}
+            />
+            <g stroke="currentColor" stroke-linecap="round" stroke-width="2">
+                <path
+                    fill="currentColor"
+                    fill-opacity="0"
+                    stroke-dasharray="34"
+                    stroke-dashoffset="34"
+                    d="M12 7C14.76 7 17 9.24 17 12C17 14.76 14.76 17 12 17C9.24 17 7 14.76 7 12C7 9.24 9.24 7 12 7"
+                >
+                    <animate
+                        fill="freeze"
+                        attributeName="stroke-dashoffset"
+                        begin="sunny-filled-entrypoint.begin"
+                        dur="0.2s"
+                        values="34;0"
+                    />
+                    <animate
+                        fill="freeze"
+                        attributeName="fill-opacity"
+                        begin="sunny-filled-entrypoint.begin+0.45s"
+                        dur="0.25s"
+                        values="0;1"
+                    />
+                </path>
+                <g fill="none" stroke-dasharray="2" stroke-dashoffset="2">
+                    <path d="M0 0">
+                        <animate
+                            fill="freeze"
+                            attributeName="d"
+                            begin="sunny-filled-entrypoint.begin+0.25s"
+                            dur="0.1s"
+                            values="M12 19v1M19 12h1M12 5v-1M5 12h-1;M12 21v1M21 12h1M12 3v-1M3 12h-1"
+                        />
+                        <animate
+                            fill="freeze"
+                            attributeName="stroke-dashoffset"
+                            begin="sunny-filled-entrypoint.begin+0.25s"
+                            dur="0.1s"
+                            values="2;0"
+                        />
+                    </path>
+                    <path d="M0 0">
+                        <animate
+                            fill="freeze"
+                            attributeName="d"
+                            begin="sunny-filled-entrypoint.begin+0.35s"
+                            dur="0.1s"
+                            values="M17 17l0.5 0.5M17 7l0.5 -0.5M7 7l-0.5 -0.5M7 17l-0.5 0.5;M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5"
+                        />
+                        <animate
+                            fill="freeze"
+                            attributeName="stroke-dashoffset"
+                            begin="sunny-filled-entrypoint.begin+0.35s"
+                            dur="0.1s"
+                            values="2;0"
+                        />
+                    </path>
+                </g>
+            </g>
+        </svg>
+    );
+});
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,34 +1,25 @@
 import rss from '@astrojs/rss';
 import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
 import MarkdownIt from 'markdown-it';
 import sanitizeHtml from 'sanitize-html';
-import { Posts } from '~/domain/values/post';
 import { PAGE_DESCRIPTION, PAGE_TITLE } from '~/utils/constants';
 
 const parser = new MarkdownIt();
 
 export const GET = async (context: APIContext) => {
     const site = context.site?.toString() ?? '';
-    const posts = await Posts.fetch();
+    const posts = await getCollection('posts');
 
     return rss({
         customData: '<language>ja</language>',
         description: PAGE_DESCRIPTION,
-        items: posts.map(
-            ({
-                firstThreeSentences,
-                rawPublished,
-                textContent,
-                title,
-                url,
-            }) => ({
-                content: sanitizeHtml(parser.render(textContent)),
-                description: firstThreeSentences,
-                link: url,
-                pubDate: rawPublished,
-                title: title,
-            }),
-        ),
+        items: posts.map(({ body, data, slug }) => ({
+            content: sanitizeHtml(parser.render(body)),
+            link: `/posts/${slug}`,
+            pubDate: data.published,
+            title: data.title,
+        })),
         site,
         title: PAGE_TITLE,
     });
