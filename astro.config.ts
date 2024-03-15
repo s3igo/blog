57277a6ee9d62d5a6a7e57f596ff821c9802ad1c
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-15 11:38:05 +0900

    feat: update icon

--- astro.config.ts
+++ astro.config.ts
@@ -3,16 +3,17 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import qwikdev from '@qwikdev/astro';
+import icon from 'astro-icon';
 import { remarkStripLineBreaks } from './remark-strip-line-breaks';
 
+// https://astro.build/config
 export default defineConfig({
     integrations: [
         qwikdev(),
-        tailwind({
-            applyBaseStyles: false,
-        }),
+        tailwind({ applyBaseStyles: false }),
         compress(),
         sitemap(),
+        icon({ include: { gg: ['dark-mode'], lucide: ['rss'] } }),
     ],
     markdown: {
         remarkPlugins: [remarkStripLineBreaks, 'remark-code-titles'],
