57454123d005f413ec96b2389059ca5d763f2311
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-16 01:32:32 +0900

    wip: navigation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,18 +5,20 @@ const config: Config = {
     content: ['./src/**/*.{astro,tsx}'],
     darkMode: 'class',
     theme: {
-        colors: {
-            'bellflower-blue': colors.slate[200],
-            'black-knight': '#030712', // gray-950
-            'blue-blouse': colors.slate[400],
-            'dreamy-cloud': colors.gray[200],
-            encore: colors.gray[500],
-            maldives: colors.cyan[500],
-            'midnight-express': colors.gray[800],
-            'spiced-nectarine': colors.orange[300],
-            transparent: 'transparent',
-            white: colors.white,
-            'yamagami-blue': '#384B5A',
+        extend: {
+            colors: {
+                'bellflower-blue': colors.slate[200],
+                'black-knight': '#030712', // gray-950
+                'blue-blouse': colors.slate[400],
+                'dreamy-cloud': colors.gray[200],
+                encore: colors.gray[500],
+                maldives: colors.cyan[500],
+                'midnight-express': colors.gray[800],
+                'spiced-nectarine': colors.orange[300],
+                transparent: 'transparent',
+                white: colors.white,
+                'yamagami-blue': '#384B5A',
+            },
         },
         fontFamily: {
             code: ['JetBrains Mono', 'monospace', 'sans-serif'],
