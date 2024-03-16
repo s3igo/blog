60c7bca196e41e84d7ace54f210e4c70b9476eb9
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-16 18:45:28 +0900

    refactor: remove duplicate codes

---
+++ src/pages/_posts.ts
@@ -0,0 +1,14 @@
+import { type CollectionEntry, getCollection } from 'astro:content';
+
+export type PostsData = {
+    posts: CollectionEntry<'posts'>[];
+    tags: string[];
+};
+
+export const getPosts = async (): Promise<PostsData> => {
+    const posts = await getCollection('posts');
+    return {
+        posts,
+        tags: [...new Set(posts.flatMap(({ data }) => data.tags).toSorted())],
+    };
+};
