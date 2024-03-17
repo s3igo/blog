50ed8a7cd284ac1f64f37fed34e19f8834ff0cb9
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-17 18:56:52 +0900

    wip

--- src/pages/_posts.ts
+++ src/pages/_posts.ts
@@ -1,8 +1,17 @@
 import { type CollectionEntry, getCollection } from 'astro:content';
 
+export type Cards = Array<{
+    title: string;
+    href: string;
+    published: string;
+    updated: string | undefined;
+    tags: string[];
+}>;
+
 type PostsData = {
     posts: CollectionEntry<'posts'>[];
     tags: string[];
+    cards: Cards;
 };
 
 export const getPosts = async (): Promise<PostsData> => {
@@ -10,5 +19,15 @@ export const getPosts = async (): Promise<PostsData> => {
     return {
         posts,
         tags: [...new Set(posts.flatMap(({ data }) => data.tags).toSorted())],
+        cards: posts
+            .map(({ data, slug }) => ({
+                title: data.title,
+                href: `/posts/${slug}`,
+                published: data.published.toISOString().substring(0, 10),
+                updated: data.updated?.toISOString().substring(0, 10),
+                tags: data.tags,
+            }))
+            .toSorted((a, b) => a.published.localeCompare(b.published))
+            .toReversed()
     };
 };
