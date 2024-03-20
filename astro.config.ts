5978a13aaea83a635a6ae0cdb54e42502e76091b
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-20 22:44:53 +0900

    feat: update post style

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,20 @@ import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import expressiveCode from 'astro-expressive-code';
 import icon from 'astro-icon';
 import { defineConfig, squooshImageService } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
+import colors from 'tailwindcss/colors';
 import { externalLinks, stripLineBreaks } from './plugins/remark';
+
 const site = 'https://blog.tsuki-yo.net';
+
+const textBg = (theme: string) =>
+    theme === 'material-theme'
+        ? '#030712' // black-knight
+        : colors.slate[100];
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,6 +36,26 @@ export default defineConfig({
             },
         }),
         partytown(),
+        expressiveCode({
+            themes: ['material-theme', 'material-theme-lighter'],
+            useThemedScrollbars: false,
+            styleOverrides: {
+                borderRadius: '0.5rem', // rounded-lg
+                codeBackground: ({ theme }) => textBg(theme.name),
+                frames: {
+                    editorActiveTabBackground: ({ theme }) =>
+                        textBg(theme.name),
+                    editorActiveTabBorderColor: ({ theme }) =>
+                        textBg(theme.name),
+                    editorTabBarBackground: ({ theme }) =>
+                        theme.name === 'material-theme'
+                            ? '#384B5A' // yamagami-blue
+                            : colors.slate[200], // bellflower-blue
+                    editorActiveTabIndicatorBottomColor: colors.cyan[500], // maldives
+                    shadowColor: 'transparent',
+                },
+            },
+        }),
     ],
     markdown: {
         remarkPlugins: [
