259b61c84ab66e96652ca785b57931ed4075eab3
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-23 22:39:12 +0900

    feat: remove color extends

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,18 +5,20 @@ const config: Config = {
     content: ['./src/**/*.{astro,tsx}'],
     darkMode: 'class',
     theme: {
-        extend: {
-            colors: {
-                'bellflower-blue': colors.slate[200],
-                'black-knight': '#030712', // gray-950
-                'blue-blouse': colors.slate[400],
-                'dreamy-cloud': colors.gray[200],
-                encore: colors.gray[500],
-                maldives: colors.cyan[500],
-                'midnight-express': colors.gray[800],
-                'spiced-nectarine': colors.orange[300],
-                'yamagami-blue': '#384B5A',
-            },
+        colors: {
+            'aphrodites-pearls': colors.cyan[50],
+            'bellflower-blue': colors.slate[200],
+            'black-knight': '#030712', // gray-950
+            'blue-blouse': colors.slate[400],
+            'dreamy-cloud': colors.gray[200],
+            encore: colors.gray[500],
+            maldives: colors.cyan[500],
+            'midnight-express': colors.gray[800],
+            'midnight-navy': colors.gray[700],
+            'spiced-nectarine': colors.orange[300],
+            'yamagami-blue': '#384B5A',
+            white: colors.white,
+            transparent: 'transparent',
         },
         fontFamily: {
             code: ['JetBrains Mono', 'monospace', 'sans-serif'],
