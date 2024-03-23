f49ed5dff8b6a32e634e216f151f2452e22e7ddc
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-23 22:41:29 +0900

    feat: split color config

---
+++ styles/colors.ts
@@ -0,0 +1,17 @@
+import colors from 'tailwindcss/colors';
+
+export const colorLiterals = {
+    'aphrodites-pearls': colors.cyan[50],
+    'bellflower-blue': colors.slate[200],
+    'black-knight': '#030712', // gray-950
+    'blue-blouse': colors.slate[400],
+    'dreamy-cloud': colors.gray[200],
+    encore: colors.gray[500],
+    maldives: colors.cyan[500],
+    'midnight-express': colors.gray[800],
+    'midnight-navy': colors.gray[700],
+    'spiced-nectarine': colors.orange[300],
+    'yamagami-blue': '#384B5A',
+    white: colors.white,
+    transparent: 'transparent',
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,11 @@
 import type { Config } from 'tailwindcss';
-import colors from 'tailwindcss/colors';
+import { colorLiterals } from './styles/colors';
 
 const config: Config = {
     content: ['./src/**/*.{astro,tsx}'],
     darkMode: 'class',
     theme: {
-        colors: {
-            'aphrodites-pearls': colors.cyan[50],
-            'bellflower-blue': colors.slate[200],
-            'black-knight': '#030712', // gray-950
-            'blue-blouse': colors.slate[400],
-            'dreamy-cloud': colors.gray[200],
-            encore: colors.gray[500],
-            maldives: colors.cyan[500],
-            'midnight-express': colors.gray[800],
-            'midnight-navy': colors.gray[700],
-            'spiced-nectarine': colors.orange[300],
-            'yamagami-blue': '#384B5A',
-            white: colors.white,
-            transparent: 'transparent',
-        },
+        colors: colorLiterals,
         fontFamily: {
             code: ['JetBrains Mono', 'monospace', 'sans-serif'],
             sans: ['Zen Kaku Gothic New', 'system-ui', 'sans-serif'],
