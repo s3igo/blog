354ed6e9b3f4524a0f9683ef89d5e002cbbb7a7d
Seigo Mori <85787242+s3igo@users.noreply.github.com>
2024-06-17 22:30:52 +0900

    Update VRT (#628)
    * feat: add lost-pixel
    
    * feat: add paths
    
    * chore: remove reg-suit
    
    * chore: remove wrangler
    
    * chore: remove default-trusted pkgs

---
+++ lostpixel.config.ts
@@ -0,0 +1,33 @@
+import type { CustomProjectConfig } from 'lost-pixel';
+
+export const config: CustomProjectConfig = {
+    pageShots: {
+        pages: [
+            {
+                path: '/',
+                name: 'index',
+            },
+            {
+                path: '/',
+                name: 'index-masked',
+            },
+            {
+                path: '/tags/astro',
+                name: 'astro',
+            },
+            {
+                path: '/tags/programming',
+                name: 'programming',
+            },
+            {
+                path: '/tags/typescript',
+                name: 'typescript',
+            },
+        ],
+        mask: [{ selector: 'svg' }],
+        breakpoints: [375, 768, 1024, 1440],
+        baseUrl: 'http://172.17.0.1:4321',
+    },
+    lostPixelProjectId: 'clxiy1du32ug0n6c9vsgtxahk',
+    apiKey: process.env.LOST_PIXEL_API_KEY,
+};
