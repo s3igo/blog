394238110712a7a2f08616200233f2d9cbd05b38
s3igo <85787242+s3igo@users.noreply.github.com>
2024-06-17 22:12:14 +0900

    feat: add paths

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,30 @@ import type { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
     pageShots: {
-        pages: [{ path: '/', name: 'index' }],
+        pages: [
+            {
+                path: '/',
+                name: 'index',
+            },
+            {
+                path: '/',
+                name: 'index-masked',
+            },
+            {
+                path: '/tags/astro',
+                name: 'astro',
+            },
+            {
+                path: '/tags/programming',
+                name: 'programming',
+            },
+            {
+                path: '/tags/typescript',
+                name: 'typescript',
+            },
+        ],
+        mask: [{ selector: 'svg' }],
+        breakpoints: [375, 768, 1024, 1440],
         baseUrl: 'http://172.17.0.1:4321',
     },
     lostPixelProjectId: 'clxiy1du32ug0n6c9vsgtxahk',
