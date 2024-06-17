ef6067d7e4d3eb48b8994df84761a9dfffaba645
s3igo <85787242+s3igo@users.noreply.github.com>
2024-06-17 17:44:56 +0900

    feat: update favicon from png to svg

---
+++ src/pages/manifest.webmanifest.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from 'astro';
+import icon192 from '~/assets/icon-192.png';
+import icon512 from '~/assets/icon-512.png';
+
+export const GET: APIRoute = () => {
+    return new Response(
+        JSON.stringify({
+            name: 'tsukiyo-room',
+            icons: [
+                { src: icon192.src, type: 'image/png', sizes: '192x192' },
+                { src: icon512.src, type: 'image/png', sizes: '512x512' },
+            ],
+        }),
+    );
+};
